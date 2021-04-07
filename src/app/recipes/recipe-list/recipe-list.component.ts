90462e71c82a26ef9494570e0e6cf1180ae6d2ed
Paul <paul@beambient.co.uk>
2021-04-07 15:19:58 +0100

    Everything up to Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg'),
-    new Recipe('A Test Recipe', 'This is simply test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg')
+    new Recipe('Another Test Recipe', 'This is simply test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg')
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
